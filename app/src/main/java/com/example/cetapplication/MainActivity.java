0ba2388bf2bd15598b8b0b3b5ec434af25c31666
JoannaCodes <joannalara21@gmail.com>
2021-05-27 16:57:08 +0800

    Main Screen UI Transition to Unit Conversion screen

--- app/src/main/java/com/example/cetapplication/MainActivity.java
+++ app/src/main/java/com/example/cetapplication/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.cetapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button UniCon_Btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //transition from main screen to unit conversion screen
+        UniCon_Btn = findViewById(R.id.UniCon_Btn);
+
+        //UniCon_Btn.setOnClickListener(new View.OnClickListener() {
+        //            @Override
+        //            public void onClick(View v) {
+        //                openUnitConversion();
+        //            }
+        //        });
+        UniCon_Btn.setOnClickListener(v -> openUnitConversion());
+    }
+
+    public void openUnitConversion() {
+        Intent intent = new Intent(this, UnitConversionActivity.class);
+        startActivity(intent);
     }
 }
