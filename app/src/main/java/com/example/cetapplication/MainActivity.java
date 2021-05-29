ff40d382d75c872dd91a8f9f4982f7dfe5a1c518
JoannaCodes <joannalara21@gmail.com>
2021-05-30 00:14:02 +0800

    activity_unitconversion.xml layout update

--- app/src/main/java/com/example/cetapplication/MainActivity.java
+++ app/src/main/java/com/example/cetapplication/MainActivity.java
@@ -3,20 +3,16 @@ package com.example.cetapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.graphics.Typeface;
 
 public class MainActivity extends AppCompatActivity {
-    private Button UniCon_Btn, Formula_Btn, MinGme_Btn;
+    Button UniCon_Btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        UniCon_Btn = findViewById(R.id.UniCon_Btn);
-        Formula_Btn = findViewById(R.id.Formula_Btn);
-        MinGme_Btn = findViewById(R.id.MinGme_Btn);
+        UniCon_Btn = findViewById(R.id.uniCon_Btn);
 
         //transition from main screen to unit conversion screen
         //UniCon_Btn.setOnClickListener(new View.OnClickListener() {
