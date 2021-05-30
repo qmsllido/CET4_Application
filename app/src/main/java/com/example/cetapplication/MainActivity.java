c3586b29693aea44b4417848bebd85f9de2fe98e
Joanna Marie <joannalara21@gmail.com>
2021-05-30 14:25:01 +0800

    activity_formula.xml layout update

--- app/src/main/java/com/example/cetapplication/MainActivity.java
+++ app/src/main/java/com/example/cetapplication/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button UniCon_Btn, MinGme_Btn;
+    Button UniCon_Btn, MinGme_Btn, Formula_Btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,25 +15,22 @@ public class MainActivity extends AppCompatActivity {
 
         UniCon_Btn = findViewById(R.id.uniCon_Btn);
         MinGme_Btn = findViewById(R.id.minGme_Btn);
+        Formula_Btn = findViewById(R.id.formula_Btn);
 
-        //transition from main screen to unit conversion screen
-        //UniCon_Btn.setOnClickListener(new View.OnClickListener() {
-        //            @Override
-        //            public void onClick(View v) {
-        //                openUnitConversion();
-        //            }
-        //        });
+        //transition from main screen to unit conversion screen -- lambda function
         UniCon_Btn.setOnClickListener(v -> openUnitConversion());
+        Formula_Btn.setOnClickListener(v -> openFormulas());
         MinGme_Btn.setOnClickListener(v -> openMiniGames());
     }
 
+    //activity trigger
     public void openUnitConversion() {
         Intent intent = new Intent(this, UnitConversionActivity.class);
         startActivity(intent);
     }
 
     public void openFormulas(){
-        Intent intent = new Intent(this, MiniGamesActivity.class);
+        Intent intent = new Intent(this, FormulaActivity.class);
         startActivity(intent);
     }
 
